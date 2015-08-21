47719f1320e8f5e3dbd27c30998a1b7aaa694c3e
HansChristian <nenseth@gmail.com>
2015-08-21 14:37:13 +0200

    Question Class

---
+++ app/src/main/java/s236334/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package s236334.geoquiz;
+
+/**
+ * Created by HansChristian on 21.08.2015.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
